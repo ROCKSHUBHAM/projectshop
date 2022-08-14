38125feb4e681b5e85719148c24302cfad6c615a
ROCKSHUBHAM <shubhampp91368136@gmail.com>
2022-08-14 15:43:59 +0530

    add silder in project

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,65 +5,48 @@ import Products from './Products'
 const Home = () => {
     return (
         <>
-            <div id="carouselExampleCaptions1" class="carousel slide relative pt-16 md:pt-7" data-bs-ride="carousel">
-                <div class="carousel-indicators absolute top-0 right-0 bottom-0 left-0 flex justify-center">
-                    <button
-                        type="button"
-                        data-bs-target="#carouselExampleCaptions1"
-                        data-bs-slide-to="0"
-                        class="active"
-                        aria-current="true"
-                        aria-label="Slide 1"
-                    ></button>
-                    <button
-                        type="button"
-                        data-bs-target="#carouselExampleCaptions1"
-                        data-bs-slide-to="1"
-                        aria-label="Slide 2"
-                    ></button>
-                    <button
-                        type="button"
-                        data-bs-target="#carouselExampleCaptions1"
-                        data-bs-slide-to="2"
-                        aria-label="Slide 3"
-                    ></button>
-                </div>
+            <div id="carouselExampleControls" class="carousel slide relative pt-16" data-bs-ride="carousel">
                 <div class="carousel-inner relative w-full overflow-hidden">
                     <div class="carousel-item active relative float-left w-full">
                         <img
-                            src="https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg"
+                            src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp"
                             class="block w-full"
-                            alt="..."
+                            alt="Wild Landscape"
                         />
-                        <div class="carousel-caption hidden md:block absolute text-center">
-                            <h5 class="text-xl">First slide label</h5>
-                            <p>Some representative placeholder content for the first slide.</p>
-                        </div>
                     </div>
                     <div class="carousel-item relative float-left w-full">
                         <img
-                            src="https://mdbootstrap.com/img/Photos/Slides/img%20(22).jpg"
+                            src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp"
                             class="block w-full"
-                            alt="..."
+                            alt="Camera"
                         />
-                        <div class="carousel-caption hidden md:block absolute text-center">
-                            <h5 class="text-xl">Second slide label</h5>
-                            <p>Some representative placeholder content for the second slide.</p>
-                        </div>
                     </div>
                     <div class="carousel-item relative float-left w-full">
                         <img
-                            src="https://mdbootstrap.com/img/Photos/Slides/img%20(23).jpg"
+                            src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp"
                             class="block w-full"
-                            alt="..."
+                            alt="Exotic Fruits"
                         />
-                        <div class="carousel-caption hidden md:block absolute text-center">
-                            <h5 class="text-xl">Third slide label</h5>
-                            <p>Some representative placeholder content for the third slide.</p>
-                        </div>
                     </div>
                 </div>
-
+                <button
+                    class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+                    type="button"
+                    data-bs-target="#carouselExampleControls"
+                    data-bs-slide="prev"
+                >
+                    <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button
+                    class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+                    type="button"
+                    data-bs-target="#carouselExampleControls"
+                    data-bs-slide="next"
+                >
+                    <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
             </div>
             <Category />
         </>
